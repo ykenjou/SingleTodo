6caeeba9d685b2e53301a97ba0941704b968fa37
ykenjou <kenjou@kenjou-no-iMac.local>
2016-08-31 03:39:54 +0900

    new setting

---
+++ SimpleTodo/PurchaseViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PurchaseViewController.swift
+//  SimpleTodo
+//
+//  Created by kenjou yutaka on 2016/08/28.
+//  Copyright © 2016年 yutaka kenjo. All rights reserved.
+//
+
+import UIKit
+import StoreKit
+
+class PurchaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
